a1f82da961fbf150ba92095e16b01e50ad801fc6
Rockstar6928 <daikazudango1@gmail.com>
2022-07-16 13:12:27 -0500

    cambio extra

--- src/main/java/com/example/proyectogestion/repository/impl/ExtraRepositoryImpl.java
+++ src/main/java/com/example/proyectogestion/repository/impl/ExtraRepositoryImpl.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Repository;
 import javax.persistence.EntityManager;
 import javax.persistence.ParameterMode;
 import javax.persistence.PersistenceContext;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Repository
@@ -20,10 +22,19 @@ public class ExtraRepositoryImpl implements ExtraRepository {
     @Override
     public Map<String, Object> getClientes(Integer p_limit) {
         Session s = em.unwrap(Session.class);
-        ProcedureCall call = s.createStoredProcedureCall(Constantes.SP_LIST_CATEGORIAS);
+        ProcedureCall call = s.createStoredProcedureCall(Constantes.SP_LIST_CLIENTES);
         call.registerParameter(1,Integer.class, ParameterMode.IN);
         call.setParameter(1, p_limit);
-
-        return null;
+        call.registerParameter(2, Integer.class,ParameterMode.OUT);
+        call.registerParameter(3, String.class,ParameterMode.OUT);
+        call.registerParameter(4, Integer.class,ParameterMode.OUT);
+        call.registerParameter(5, Class.class,ParameterMode.OUT);
+        Map<String, Object> result = new HashMap<>();
+        result.put("OUT_CODIGO",call.getOutputParameterValue(2));
+        result.put("OUT_MSG",call.getOutputParameterValue(3));
+        result.put("OUT_CANTIDAD",call.getOutputParameterValue(4));
+        List<Object> list = (List<Object>) call.getResultList();
+        result.put("OUT_LIST",list);
+        return result;
     }
 }
--- src/main/java/com/example/proyectogestion/util/Constantes.java
+++ src/main/java/com/example/proyectogestion/util/Constantes.java
@@ -4,4 +4,6 @@ public class Constantes {
     public static final String CODIGO_ERROR = "9999";
     public static final String CODIGO_NO_SE_ENCONTRARON_DATOS = "0001";
     public static final String SP_LIST_CATEGORIAS = "PRUEBAS_GENERALES.SP_LIST_CATEGORIAS";
+    public static final String SP_LIST_CLIENTES = "PRUEBAS_GENERALES.SP_LIST_CLIENTES";
+    public static final String SP_LIST_PRODUCTOS = "PRUEBAS_GENERALES.SP_LIST_PRODUCTOS";
 }
