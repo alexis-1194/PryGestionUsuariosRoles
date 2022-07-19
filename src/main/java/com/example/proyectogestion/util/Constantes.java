04e3f076df88ea2012ab7617456504e134f8dc0e
Rockstar6928 <daikazudango1@gmail.com>
2022-07-19 12:21:01 -0500

    restablecer

--- src/main/java/com/example/proyectogestion/util/Constantes.java
+++ src/main/java/com/example/proyectogestion/util/Constantes.java
@@ -6,4 +6,23 @@ public class Constantes {
     public static final String SP_LIST_CATEGORIAS = "PKG_PRUEBAS_GENERALES.SP_LIST_CATEGORIAS";
     public static final String SP_LIST_CLIENTES = "PKG_PRUEBAS_GENERALES.SP_LIST_CLIENTES";
     public static final String SP_LIST_PRODUCTOS = "PKG_PRUEBAS_GENERALES.SP_LIST_PRODUCTOS";
+
+    /*
+    * #  datasource:
+    #    url: jdbc:oracle:thin:@localhost:1521/xe
+    #    username: gestion
+    #    password: gestion
+    #    url: jdbc:oracle:thin:@localhost:1521:orcl
+#    url: jdbc:oracle:thin:@localhost:1521/ORCL
+#    username: yalerolev1
+#    password: yalerolev1
+    #    username: gestion
+    #    password: gestion
+
+#    driver-class-name: oracle.jdbc.driver.OracleDriver
+#  application:
+#    name: ClientesBackend
+*
+* #    database-platform: org.hibernate.dialect.Oracle10gDialect
+    * */
 }
