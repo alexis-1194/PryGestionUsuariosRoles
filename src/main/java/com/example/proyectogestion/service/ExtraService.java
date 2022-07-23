abca24c769126d9d105a04a74068935aec448091
Rockstar6928 <daikazudango1@gmail.com>
2022-07-23 13:11:08 -0500

    todo

--- src/main/java/com/example/proyectogestion/service/ExtraService.java
+++ src/main/java/com/example/proyectogestion/service/ExtraService.java
@@ -6,4 +6,6 @@ public interface ExtraService {
     Response getClientes(Integer p_limit);
     Response getCategorias(Integer p_limit);
     Response getProductos(Integer p_limit);
+
+    //TODO: getJuegos, getAlmacen, getProveedores
 }
