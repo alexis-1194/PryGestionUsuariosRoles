85748d6d1e6fcb37bb68b6d2185794d4c53475eb
Rockstar6928 <daikazudango1@gmail.com>
2022-07-23 17:31:38 -0500

    extras sin oauth

--- src/main/java/com/example/proyectogestion/ProyectoGestionApplication.java
+++ src/main/java/com/example/proyectogestion/ProyectoGestionApplication.java
@@ -3,7 +3,7 @@ package com.example.proyectogestion;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @RequiredArgsConstructor
